1265b890a32d8acc6228cf2243a3ccd39605004c
Fedor <f.tkachov@gmail.com>
2023-08-26 14:33:45 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import { name } from "./modul";
+import "./style.css";
+import isURL from "validator/lib/isURL";
+import isUUID from "validator/lib/isUUID";
+import isVAT from "validator/lib/isVAT";
+
+console.log(name);
